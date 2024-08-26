87c206038f66b92cba7664a45329af23c56d0ce8
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-26 07:58:17 +0000

    Merge code from frontend

--- components/simple_footer_component_1724657430.js
+++ components/simple_footer_component_1724657430.js
@@ -2,13 +2,13 @@
 */
 Vue.component("simple_footer_component_1724657430", {
     template: `
-    <footer id="footer-section" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
+    <footer id="footer-section" style="min-height: 470px" class="flex-1 bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg">
             <div id="footer-container" class="max-w-screen-xl p-8 py-10 mx-auto lg:py-16 md:p-12 lg:p-14">
                 <hr id="footer-divider" class="my-8 border-pink-300 sm:mx-auto opacity-30 lg:my-10">
                 <div id="footer-content" class="text-center">
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-6 text-3xl font-bold text-white hover:text-pink-200 transition duration-300">
-                            <img id="footer-logo" src="./images/logo.svg" class="h-8 mr-4 sm:h-10" alt="Landwind Logo" />
+                            <img id="footer-logo" src="https://makeinfinite-mentat-test.azurewebsites.net/get_image/Food_App_1724657429/logo.svg" class="h-8 mr-4 sm:h-10" alt="Landwind Logo" />
                             FoodieFind
                         </a>
                     </div>
